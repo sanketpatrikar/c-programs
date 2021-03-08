1a3ac6479ba4e292c136f990de0d1c89d2750259
sanketpatrikar <sanketspatrikar@gmail.com>
2021-03-08 16:23:00 +0530

    simplify boolean logic

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -2,12 +2,13 @@
 
 int main(void)
 {
-    for (int i = 1; i <= 100; i++) {
+    for (int i = 1; i <= 100; i++)
+    {
         if (!(i % 3))
             printf("Fizz");
         if (!(i % 5))
             printf("Buzz");
-        if (i % 15)
+        if ((i % 3) && (i % 5))
             printf("%d", i);
         printf("\n");
     }
